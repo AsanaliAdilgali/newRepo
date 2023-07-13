23e13a871e804f062068ea64e183af548eac6be1
Асанали Адилгали <asanali13102006@gmail.com>
2023-07-13 15:54:28 +0600

    sortMerge

--- src/main/java/sortMerge/Solution.java
+++ src/main/java/sortMerge/Solution.java
@@ -1,45 +1,39 @@
 package sortMerge;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("");
-        int i = scanner.nextInt();
-        Solution solution = new Solution();
+        int arr1[] = {1,2,3};
 
+        int arr2[] = {2,5,6};
+
+        int mas1 = arr1.length;
+        int mas2 = arr2.length;
+        int arr3[] = new int[mas1 + mas2];
+        mergeArrays(arr1, arr2, mas1, mas2, arr3);
+        System.out.println("result: ");
+        for (int i = 0; i < mas1 + mas2; i++){
+            System.out.println(arr3[i] + " ");
+        }
     }
 
-    public int sorted(int[] nums1, int m, int[] nums2, int n) {
-        boolean isSorted = false;
-        int last1 = m - 1;
-        int last2 = n - 1;
+    public static void mergeArrays(int[] arr1, int[] arr2, int n1, int n2, int[] arr3) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
 
-        int index = nums1.length - 1;
-
-        while (last1 >= 0 && last2 >= 0) {
-            int l = nums1[last1];
-            int r = nums2[last2];
-
-            if (l < r) {
-                last2--;
-                return nums1[index--] = r;
-            } else {
-                last1--;
-                return nums1[index--] = l;
-            }
+        while (i < n1) {
+            arr3[k++] = arr1[i++];
+        }
+        while (j < n2) {
+            arr3[k++] = arr2[j++];
         }
 
-        while (last2 >= 0) {
-            int r = nums2[last2];
-
-            nums1[index--] = r;
-            last2--;
-        }
-        isSorted = true;
-        return last1;
+        Arrays.sort(arr3);
     }
 }
+
 
 
